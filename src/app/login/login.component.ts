ebd17c7abbc44feb25bb21518fa3b92b229ca084
DownloadableFox <me@downloadablefox.dev>
2025-04-29 08:40:24 -0500

    added login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,11 +13,26 @@ export class LoginComponent {
 		private readonly loginService: LoginService
 	) {}
 
+	public userType: string = 'client';
 	public email: string = '';
 	public password: string = '';
 	public errorMessage: string = '';
 
 	public login(): void {
+		switch (this.userType) {
+			case 'client':
+				this.loginClient();
+				break;
+			case 'admin':
+				this.router.navigate(['/admin']);
+				break;
+			case 'vet':
+				this.router.navigate(['/veterinarian']);
+				break;
+		}
+	}
+
+	public loginClient(): void {
 		this.loginService.login(this.email, this.password).subscribe({
 			next: () => {
 				this.router.navigate(['/client']);
