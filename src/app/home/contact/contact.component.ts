f76d6ef8eac2514cd31467cdacaf448053241a11
gyusba <f-garrido@javeriana.edu.co>
2025-05-26 21:30:23 -0500

    Correo

--- src/app/home/contact/contact.component.ts
+++ src/app/home/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'home-contact',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+  name = '';
+  email = '';
+  message = '';
+  success = false;
+  error = false;
 
+  constructor(private http: HttpClient) {}
+
+  sendMessage() {
+    const data = {
+      name: this.name,
+      email: this.email,
+      message: this.message
+    };
+
+    this.http.post('http://localhost:8080/contact', data).subscribe({
+      next: () => {
+        this.success = true;
+        this.error = false;
+        this.name = this.email = this.message = '';
+      },
+      error: () => {
+        this.success = false;
+        this.error = true;
+      }
+    });
+  }
 }
